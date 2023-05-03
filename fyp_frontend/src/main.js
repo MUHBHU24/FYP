86331c8e1093440029bec5319665ad8f5c6a7332
Muhammad Fazal Bhutawala <muhbhu@gmail.com>
2023-05-03 22:33:55 +0100

    formatting, recolour navbar

--- fyp_frontend/src/main.js
+++ fyp_frontend/src/main.js
@@ -1,13 +1,13 @@
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
-
-//import './style.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
