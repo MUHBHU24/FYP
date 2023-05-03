ced0b7df2c63fd0b7d447487082a73ea40582ff9
Muhammad Fazal Bhutawala <muhbhu@gmail.com>
2023-05-03 18:28:24 +0100

    navbar

--- fyp_frontend/src/main.js
+++ fyp_frontend/src/main.js
@@ -3,12 +3,17 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
-import './assets/main.css'
+//import './style.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(router, axios)
 
 app.mount('#app')
