76a38138d068175ab9b84c700d422ac9b3300ad8
Muhammad Fazal Bhutawala <muhbhu@gmail.com>
2023-05-03 20:55:43 +0100

    login and router

---
+++ fyp_frontend/src/views/LoginView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+      <div class="row g-5">
+        <div class="col-lg-6 main-left">
+          <div class="p-4 bg-light border border-1 border-secondary rounded">
+            <h1 class="mb-4 display-4">Login to your account!</h1>
+  
+            <p class="mb-4 fst-italic text-muted">
+              Welcome to <strong>Vocabularious</strong>!
+            </p>
+  
+            <p class="mb-4 text-muted">
+              Please enter your login credentials to access your account.
+            </p>
+  
+            <p class="fw-bold"><small>
+              Don't have an account yet? Click <RouterLink to="/register" class="text-primary"><mark>here</mark></RouterLink> to register!
+            </small>
+            </p>
+          </div>
+        </div>
+  
+        <div class="col-lg-6 main-right">
+          <div class="p-4 bg-white border border-1 border-secondary rounded">
+            <form>
+              <div class="mb-3">
+                <label class="form-label">Username</label>
+                <input type="text" placeholder="Enter your username..." class="form-control">
+              </div>
+  
+              <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input type="password" placeholder="Enter your password..." class="form-control">
+              </div>
+
+              <div>
+                <button class="btn btn-primary">Log in</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
--- fyp_frontend/src/views/RegisterView.vue
+++ fyp_frontend/src/views/RegisterView.vue
@@ -1,56 +1,57 @@
 <template>
-    <div class="container mx-auto">
-      <div class="row g-4">
-        <div class="col main-left">
-          <div class="p-4 bg-white border border-1 border-secondary rounded">
-            <h1 class="mb-4 fs-3">Register to become a member!</h1>
+    <div class="container">
+      <div class="row g-5">
+        <div class="col-lg-6 main-left">
+          <div class="p-4 bg-light border border-1 border-secondary rounded">
+            <h1 class="mb-4 display-4">Register to become a Vocabularious member!</h1>
+  
+            <p class="mb-4 fst-italic text-muted">
+              Welcome to <strong>Vocabularious</strong>!
+            </p>
   
             <p class="mb-4 text-muted">
-              Welcome to <strong>Vocabularious</strong>! 
-            </p>
-
-            <p class="mb-4 text-muted">
-            Please fill in the form provided to register as a member.
+              Please fill in the form provided to register as a member.
               By signing up, you will have full access to our site and be able to contribute to numerous surveys.
             </p>
   
             <p class="mb-4 text-muted">
               Regardless of whether you are a seasoned researcher or a curious individual interested in exploring the field of lexical variation,
-               we welcome you to take interest and join us!
+              we welcome you to take interest and join us!
             </p>
-
-            <p class="fw-bold">
-              Already have an account? Click here to log in!
+  
+            <p class="fw-bold"><small>
+              Already have an account? Click <RouterLink to="/login" class="text-primary"><mark>here</mark></RouterLink> to log in!
+            </small>
             </p>
           </div>
         </div>
   
-        <div class="col main-right">
+        <div class="col-lg-6 main-right">
           <div class="p-4 bg-white border border-1 border-secondary rounded">
             <form>
-              <div class="mb-4">
-                <label>Name</label><br>
-                <input type="text" placeholder="Enter your name..." class="form-control mt-2">
-              </div>
-
-              <div class="mb-4">
-                <label>City</label><br>
-                <input type="text" placeholder="Your location..." class="form-control mt-2">
+              <div class="mb-3">
+                <label class="form-label">Name</label>
+                <input type="text" placeholder="Enter your name..." class="form-control">
               </div>
   
-              <div class="mb-4">
-                <label>Username</label><br>
-                <input type="text" placeholder="Create a username..." class="form-control mt-2">
+              <div class="mb-3">
+                <label class="form-label">City</label>
+                <input type="text" placeholder="Your location..." class="form-control">
               </div>
   
-              <div class="mb-4">
-                <label>Password</label><br>
-                <input type="password" placeholder="Create a password..." class="form-control mt-2">
+              <div class="mb-3">
+                <label class="form-label">Username</label>
+                <input type="text" placeholder="Create a username..." class="form-control">
               </div>
   
-              <div class="mb-4">
-                <label>Repeat password</label><br>
-                <input type="password" placeholder="Confirm password..." class="form-control mt-2">
+              <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input type="password" placeholder="Create a password..." class="form-control">
+              </div>
+  
+              <div class="mb-3">
+                <label class="form-label">Repeat password</label>
+                <input type="password" placeholder="Confirm password..." class="form-control">
               </div>
   
               <div>
