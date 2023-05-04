2cc0cec28a1297402d50d0b51dcab26877eee514
Muhammad Fazal Bhutawala <muhbhu@gmail.com>
2023-05-04 13:33:19 +0100

    Update toast.js

--- fyp_frontend/src/stores/toast.js
+++ fyp_frontend/src/stores/toast.js
@@ -17,12 +17,12 @@ export const useToastStore = defineStore({
 
       // animate the toast to slide up
       setTimeout(() => {
-        this.classes += "-translate-y-28";
+        this.classes += "translate-y-3";
       }, 20);
 
       // animate the toast to slide down after the specified duration
       setTimeout(() => {
-        this.classes = this.classes.replace("-translate-y-28", "");
+        this.classes = this.classes.replace("translate-y-3", "");
       }, this.ms - 750);
 
       // hide the toast after the specified duration
