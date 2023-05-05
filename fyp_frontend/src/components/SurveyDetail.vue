c9f4ab4a3d150528531cf5a2f3033fbe43ccec1d
Muhammad Fazal Bhutawala <muhbhu@gmail.com>
2023-05-06 00:19:36 +0100

    format

--- fyp_frontend/src/components/SurveyDetail.vue
+++ fyp_frontend/src/components/SurveyDetail.vue
@@ -1,19 +1,17 @@
 <script>
-import SurveyComments from './SurveyComments.vue';
+import SurveyComments from "./SurveyComments.vue";
 
 export default {
-  components: {
-    SurveyComments,
-  },
-  data() {
-    return {
-      surveyId: null, // This should be set to the actual survey ID
-    };
-  },
-  // other methods and lifecycle hooks
+	components: {
+		SurveyComments,
+	},
+	data() {
+		return {
+			surveyId: null, // This should be set to the actual survey ID
+		};
+	},
+	// other methods and lifecycle hooks
 };
 </script>
 
-<template>
-    
-</template>
+<template></template>
