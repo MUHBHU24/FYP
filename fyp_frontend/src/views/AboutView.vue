16d80288da85338de4c7214e3de84c565139b3ac
Muhammad Fazal Bhutawala <muhbhu@gmail.com>
2023-05-06 00:25:18 +0100

    format

--- fyp_frontend/src/views/AboutView.vue
+++ fyp_frontend/src/views/AboutView.vue
@@ -1,225 +1,235 @@
 <script>
 export default {
-  name: "AboutView",
+    name: "AboutView",
 };
 </script>
 
 <template>
-  <div class="container">
-    <div class="row mt-5">
-      <div class="col-12 text-center">
-        <h1 class="display-4 text-secondary">About Vocabularious</h1>
-      </div>
-    </div>
-    <div class="row mt-5">
-      <div class="col-lg-4 col-12 mb-3">
-        <div class="embed-responsive embed-responsive-16by9">
-          <iframe
-            class="embed-responsive-item"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9581951.253777225!2d-15.00464572445363!3d54.10311305732506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x25a3b1142c791a9%3A0xc4f8a0433288257a!2sUnited%20Kingdom!5e0!3m2!1sen!2suk!4v1683158841160!5m2!1sen!2suk"
-            style="border: 0"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-12 text-center">
+                <h1 class="display-4 text-secondary">About Vocabularious</h1>
+            </div>
         </div>
-      </div>
-      <div class="col-lg-4 col-12">
-        <h2 class="h4 text-primary fw-bold mb-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-check2-square"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"
-            />
-            <path
-              d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"
-            />
-          </svg>
-          Our Platform
-        </h2>
-        <p class="text-muted">
-          Vocabularious is a platform dedicated to the study of language and
-          dialects. We provide a comprehensive resource for linguistic
-          enthusiasts and researchers to explore and understand the complexities
-          of language variation.
-        </p>
-        <h2 class="h4 text-primary fw-bold mb-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-check2-square"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"
-            />
-            <path
-              d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"
-            />
-          </svg>
-          Google Maps Integration
-        </h2>
-        <p class="text-muted">
-          The embedded Google Maps displayed on this page is interactive to help
-          you locate which city you live in. The dotted red outline represents
-          which regions are currently being studied on this website (the UK).
-        </p>
-      </div>
-      <div class="col-lg-4 col-12">
-        <h2 class="h4 text-primary fw-bold mb-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-check2-square"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"
-            />
-            <path
-              d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"
-            />
-          </svg>
-          Our Mission
-        </h2>
-        <p class="text-muted">
-          To help foster a global community where people can share their
-          knowledge, insights, and experiences about language and dialects. We
-          aim to make the study of language more accessible, exciting and
-          engaging for everyone.
-        </p>
+        <div class="row mt-5">
+            <div class="col-lg-4 col-12 mb-3">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe
+                        class="embed-responsive-item"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9581951.253777225!2d-15.00464572445363!3d54.10311305732506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x25a3b1142c791a9%3A0xc4f8a0433288257a!2sUnited%20Kingdom!5e0!3m2!1sen!2suk!4v1683158841160!5m2!1sen!2suk"
+                        style="border: 0"
+                        allowfullscreen=""
+                        loading="lazy"
+                        referrerpolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                </div>
+            </div>
+            <div class="col-lg-4 col-12">
+                <h2 class="h4 text-primary fw-bold mb-3">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-check2-square"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"
+                        />
+                        <path
+                            d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"
+                        />
+                    </svg>
+                    Our Platform
+                </h2>
+                <p class="text-muted">
+                    Vocabularious is a platform dedicated to the study of
+                    language and dialects. We provide a comprehensive resource
+                    for linguistic enthusiasts and researchers to explore and
+                    understand the complexities of language variation.
+                </p>
+                <h2 class="h4 text-primary fw-bold mb-3">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-check2-square"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"
+                        />
+                        <path
+                            d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"
+                        />
+                    </svg>
+                    Google Maps Integration
+                </h2>
+                <p class="text-muted">
+                    The embedded Google Maps displayed on this page is
+                    interactive to help you locate which city you live in. The
+                    dotted red outline represents which regions are currently
+                    being studied on this website (the UK).
+                </p>
+            </div>
+            <div class="col-lg-4 col-12">
+                <h2 class="h4 text-primary fw-bold mb-3">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-check2-square"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            d="M3 14.5A1.5 1.5 0 0 1 1.5 13V3A1.5 1.5 0 0 1 3 1.5h8a.5.5 0 0 1 0 1H3a.5.5 0 0 0-.5.5v10a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8a.5.5 0 0 1 1 0v5a1.5 1.5 0 0 1-1.5 1.5H3z"
+                        />
+                        <path
+                            d="m8.354 10.354 7-7a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"
+                        />
+                    </svg>
+                    Our Mission
+                </h2>
+                <p class="text-muted">
+                    To help foster a global community where people can share
+                    their knowledge, insights, and experiences about language
+                    and dialects. We aim to make the study of language more
+                    accessible, exciting and engaging for everyone.
+                </p>
 
-        <h2 class="h4 text-primary fw-bold mb-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-exclamation-diamond"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.482 1.482 0 0 1 0-2.098L6.95.435zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"
-            />
-            <path
-              d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"
-            />
-          </svg>
-          Disclaimer
-        </h2>
-        <p class="text-muted">
-          <small
-            ><em
-              >Please note: Vocabularious is solely focused on the theme of
-              lexical variation in regions of the UK at this point in time.</em
-            ></small
-          >
-        </p>
-        <button
-          type="button"
-          class="btn btn-primary btn-sm mt-3 mb-3 text-white fw-bold fs-6 px-3 py-2 rounded-pill w-100"
-          data-bs-toggle="modal"
-          data-bs-target="#lexicalVariationModal"
-        >
-          What exactly is lexical variation anyway?
-        </button>
-      </div>
-    </div>
-    <!-- Lexical Variation Modal -->
-    <div
-      class="modal fade"
-      id="lexicalVariationModal"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="lexicalVariationModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title fw-bold" id="lexicalVariationModalLabel">
-              Lexical Variation
-            </h5>
-            <button
-              type="button"
-              class="close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            Variations in vocabulary, known as lexical variation, arise among
-            different dialects, regions, or social groups, resulting in unique
-            words, phrases, or expressions that convey similar meanings. This
-            natural aspect of language evolution allows us to gain insights into
-            the historical background, cultural influences, and social dynamics
-            of speakers. Studying lexical variation enables researchers to delve
-            deeper into the forces that shape linguistic change and the factors
-            that contribute to the diversity of languages. Investigating and
-            documenting lexical variation not only helps preserve our linguistic
-            heritage but also encourages understanding and appreciation for the
-            complexity and richness of human language.
-            <br /><br />
-            Common examples: <br /><br />
-            (1) Regional variations: Vocabulary differences based on
-            geographical location, often influenced by a combination of
-            historical, cultural, and social factors. <br /><br />
-            (2) Social variations: Vocabulary distinctions that stem from
-            various social factors, such as age, gender, education, and
-            socioeconomic status. <br /><br />
-            (3) Temporal variations: Vocabulary changes that develop over time,
-            reflecting ongoing advancements in culture, technology, and society.
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-danger text-white"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            >
-              Close
-            </button>
-          </div>
+                <h2 class="h4 text-primary fw-bold mb-3">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-exclamation-diamond"
+                        viewBox="0 0 16 16"
+                    >
+                        <path
+                            d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.482 1.482 0 0 1 0-2.098L6.95.435zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z"
+                        />
+                        <path
+                            d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"
+                        />
+                    </svg>
+                    Disclaimer
+                </h2>
+                <p class="text-muted">
+                    <small
+                        ><em
+                            >Please note: Vocabularious is solely focused on the
+                            theme of lexical variation in regions of the UK at
+                            this point in time.</em
+                        ></small
+                    >
+                </p>
+                <button
+                    type="button"
+                    class="btn btn-primary btn-sm mt-3 mb-3 text-white fw-bold fs-6 px-3 py-2 rounded-pill w-100"
+                    data-bs-toggle="modal"
+                    data-bs-target="#lexicalVariationModal"
+                >
+                    What exactly is lexical variation anyway?
+                </button>
+            </div>
         </div>
-      </div>
+        <!-- Lexical Variation Modal -->
+        <div
+            class="modal fade"
+            id="lexicalVariationModal"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="lexicalVariationModalLabel"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5
+                            class="modal-title fw-bold"
+                            id="lexicalVariationModalLabel"
+                        >
+                            Lexical Variation
+                        </h5>
+                        <button
+                            type="button"
+                            class="close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Variations in vocabulary, known as lexical variation,
+                        arise among different dialects, regions, or social
+                        groups, resulting in unique words, phrases, or
+                        expressions that convey similar meanings. This natural
+                        aspect of language evolution allows us to gain insights
+                        into the historical background, cultural influences, and
+                        social dynamics of speakers. Studying lexical variation
+                        enables researchers to delve deeper into the forces that
+                        shape linguistic change and the factors that contribute
+                        to the diversity of languages. Investigating and
+                        documenting lexical variation not only helps preserve
+                        our linguistic heritage but also encourages
+                        understanding and appreciation for the complexity and
+                        richness of human language.
+                        <br /><br />
+                        Common examples: <br /><br />
+                        (1) Regional variations: Vocabulary differences based on
+                        geographical location, often influenced by a combination
+                        of historical, cultural, and social factors.
+                        <br /><br />
+                        (2) Social variations: Vocabulary distinctions that stem
+                        from various social factors, such as age, gender,
+                        education, and socioeconomic status. <br /><br />
+                        (3) Temporal variations: Vocabulary changes that develop
+                        over time, reflecting ongoing advancements in culture,
+                        technology, and society.
+                    </div>
+                    <div class="modal-footer">
+                        <button
+                            type="button"
+                            class="btn btn-danger text-white"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
 .row > .col-md-6 {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 }
 
 .embed-responsive {
-  height: 0;
-  padding-bottom: 56.25%;
-  position: relative;
+    height: 0;
+    padding-bottom: 56.25%;
+    position: relative;
 }
 
 .embed-responsive-item {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
 }
 
 .modal-dialog {
-  max-width: 800px;
+    max-width: 800px;
 }
 </style>
