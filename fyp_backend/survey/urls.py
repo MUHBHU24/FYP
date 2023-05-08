75050564a2d9ec17729a996bbc59d90728548a3c
Muhammad Fazal Bhutawala <muhbhu@gmail.com>
2023-05-08 17:22:58 +0100

    url change

--- fyp_backend/survey/urls.py
+++ fyp_backend/survey/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('register/', api.register, name='register'), # Register
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), # Login
     path('surveys/search/', api.search_surveys, name='search_surveys'), # Search surveys
-    path('surveys/', views.getAllSurveys.as_view(), name='getAllSurveys'), # Get the most recent surveys
+    path('surveys/', api.getAllSurveys, name='getAllSurveys'), # Get the most recent surveys
     # path('/api/surveys/', api.surveys, name='surveys'), # Get all surveys
     path('refresh/', TokenRefreshView.as_view(), name='refreshToken'), # Refresh token
     # path('api/survey_comments/<int:survey_id>/', views.get_survey_comments),
